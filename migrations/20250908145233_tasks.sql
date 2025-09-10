eef4140f6bfdb57f8573d037f714b058e52d336c
William Lea <william.lea@xebia.com>
2025-09-10 15:27:35 +0100

    removed extra migration

--- migrations/20250908145233_tasks.sql
+++ migrations/20250908145233_tasks.sql
@@ -1,6 +1,6 @@
 -- Add migration script here
 CREATE TABLE IF NOT EXISTS tasks(
-    id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     title TEXT NOT NULL,
     description TEXT,
     lane TEXT NOT NULL
